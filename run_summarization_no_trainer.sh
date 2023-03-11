88bda51a72ee6d1444c896a722c964453cd42978
liupeng15 <liupeng15@kuaishou.com>
2023-03-11 15:33:30 +0800

    accelerate mt5

---
+++ run_summarization_no_trainer.sh
@@ -0,0 +1,18 @@
+accelerate launch --multi_gpu --gpu_ids "1,2,3" run_summarization_no_trainer.py \
+    --model_name_or_path google/mt5-base \
+    --dataset_name cnn_dailymail \
+    --dataset_config "3.0.0" \
+    --source_prefix "summarize: " \
+    --output_dir model/mbart-large-50-summarization \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --learning_rate 1e-4 \
+    --num_train_epochs 10 \
+    --lr_scheduler_type 'cosine' \
+    --num_warmup_steps 10000 \
+    --seed 1111 \
+    --checkpointing_steps epoch \
+    --max_length 1024 \
+    --max_target_length 128 \
+    --num_beams 1 \
+#     --with_tracking
