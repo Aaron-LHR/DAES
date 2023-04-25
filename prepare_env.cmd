f024cb1d4a721aa7b00ca0dd4c271f60b0a36300
Aaron <lianhaoran999@gmail.com>
2023-04-25 22:38:23 +0800

    env

--- prepare_env.cmd
+++ prepare_env.cmd
@@ -1,2 +1,3 @@
 pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117 -U
 pip install en_core_web_sm-3.5.0.tar.gz -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
