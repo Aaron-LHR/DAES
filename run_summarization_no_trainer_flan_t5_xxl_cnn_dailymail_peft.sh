eb3457725ae1f73f6dbdc150bd39f49213538c04
Aaron <lianhaoran999@gmail.com>
2023-05-10 16:12:58 +0800

    指定gpu

--- run_summarization_no_trainer_flan_t5_xxl_cnn_dailymail_peft.sh
+++ run_summarization_no_trainer_flan_t5_xxl_cnn_dailymail_peft.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1,2, ccelerate launch run_summarization_no_trainer.py \
+CUDA_VISIBLE_DEVICES=1,2 accelerate launch run_summarization_no_trainer.py \
     --model_name_or_path google/flan-t5-xxl \
     --dataset_name cnn_dailymail \
     --dataset_config "3.0.0" \
